7c36a3687f2d35ade5f4d09a718ee1716ee541e6
rpbrehm <Lowrider01>
2015-12-08 07:58:57 -0800

    Create initial framework

---
+++ sqs-sns/src/main/java/com.mycompany/AWSConfig.java
@@ -0,0 +1,12 @@
+package com.mycompany;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by rpbrehm on 12/8/15.
+ */
+@Configuration
+public class AWSConfig {
+
+
+}
